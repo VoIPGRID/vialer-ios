a262ee390d359c4d874e70678dce058bbba9d665
Reinier Wieringa <reinier@astralvision.nl>
2014-08-13 16:28:45 +0200

    APP-68

---
+++ Vialer/PasteableTextView.h
@@ -0,0 +1,13 @@
+//
+//  PasteableTextField.h
+//  Vialer
+//
+//  Created by Reinier Wieringa on 13/08/14.
+//  Copyright (c) 2014 VoIPGRID. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PasteableTextField : UITextField
+
+@end
---
+++ Vialer/PasteableTextView.m
@@ -0,0 +1,31 @@
+//
+//  PasteableTextField.m
+//  Vialer
+//
+//  Created by Reinier Wieringa on 13/08/14.
+//  Copyright (c) 2014 VoIPGRID. All rights reserved.
+//
+
+#import "PasteableTextField.h"
+
+@implementation PasteableTextField
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
