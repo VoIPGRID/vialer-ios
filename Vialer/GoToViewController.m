5be495d91f48027d013e65fa1c7528dfe0db5477
Reinier Wieringa <reinier@astralvision.nl>
2014-08-20 16:28:52 +0200

    APP-71

---
+++ Vialer/GoToViewController.m
@@ -0,0 +1,38 @@
+//
+//  GoToViewController.m
+//  Vialer
+//
+//  Created by Reinier Wieringa on 13/08/14.
+//  Copyright (c) 2014 VoIPGRID. All rights reserved.
+//
+
+#import "GoToViewController.h"
+
+@interface GoToViewController ()
+
+@end
+
+@implementation GoToViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
