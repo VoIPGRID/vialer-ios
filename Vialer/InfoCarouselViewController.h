bf44b1c6354d4569053f96d15a8c8914f20dd47d
Reinier Wieringa <reinier@astralvision.nl>
2014-09-22 17:32:37 +0200

    APP-72

---
+++ Vialer/InfoCarouselViewController.h
@@ -0,0 +1,13 @@
+//
+//  InfoCarouselViewController.h
+//  Vialer
+//
+//  Created by Reinier Wieringa on 09/09/14.
+//  Copyright (c) 2014 VoIPGRID. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InfoCarouselViewController : UIViewController
+
+@end
---
+++ Vialer/InfoCarouselViewController.m
@@ -0,0 +1,38 @@
+//
+//  InfoCarouselViewController.m
+//  Vialer
+//
+//  Created by Reinier Wieringa on 09/09/14.
+//  Copyright (c) 2014 VoIPGRID. All rights reserved.
+//
+
+#import "InfoCarouselViewController.h"
+
+@interface InfoCarouselViewController ()
+
+@end
+
+@implementation InfoCarouselViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
