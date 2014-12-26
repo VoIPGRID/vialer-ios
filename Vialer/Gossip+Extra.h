39a8444e276bb052531eacc4dcf633b585fd8b79
Reinier Wieringa <reinier@astralvision.nl>
2014-12-26 11:53:44 +0100

    - Fixed: SIP implementation in call screen, duration timer, pause support, speaker support, mute support

---
+++ Vialer/Gossip+Extra.h
@@ -0,0 +1,13 @@
+//
+//  GSCall+Gossip_Extra.h
+//  Vialer
+//
+//  Created by Reinier Wieringa on 26/12/14.
+//  Copyright (c) 2014 VoIPGRID. All rights reserved.
+//
+
+#import "GSCall.h"
+
+@interface GSCall (Gossip_Extra)
+
+@end
---
+++ Vialer/Gossip+Extra.m
@@ -0,0 +1,13 @@
+//
+//  GSCall+Gossip_Extra.m
+//  Vialer
+//
+//  Created by Reinier Wieringa on 26/12/14.
+//  Copyright (c) 2014 VoIPGRID. All rights reserved.
+//
+
+#import "GSCall+Gossip_Extra.h"
+
+@implementation GSCall (Gossip_Extra)
+
+@end
