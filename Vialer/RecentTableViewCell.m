b54f39243046eb8ea596749796bf6c7fd8049af3
Reinier Wieringa <reinier@astralvision.nl>
2015-04-06 22:49:01 +0200

    VIALI-30 Recents table view style adjustments

--- Vialer/RecentTableViewCell.m
+++ Vialer/RecentTableViewCell.m
@@ -24,21 +24,21 @@
         
         _nameLabel = [[UILabel alloc] init];
         self.nameLabel.backgroundColor = [UIColor clearColor];
-        self.nameLabel.font = [UIFont boldSystemFontOfSize:18.f];
+        self.nameLabel.font = [UIFont systemFontOfSize:15.f];
         self.nameLabel.numberOfLines = 1;
         self.nameLabel.textColor = [UIColor blackColor];
         self.nameLabel.opaque = YES;
 
         _descriptionLabel = [[UILabel alloc] init];
         self.descriptionLabel.backgroundColor = [UIColor clearColor];
-        self.descriptionLabel.font = [UIFont systemFontOfSize:12.f];
+        self.descriptionLabel.font = [UIFont systemFontOfSize:10.f];
         self.descriptionLabel.textColor = [UIColor colorWithRed:0x54 / 255.f green:0x58 / 255.f blue:0x6d / 255.f alpha:1.f];
         self.descriptionLabel.numberOfLines = 1;
         self.descriptionLabel.opaque = YES;
 
         _dateTimeLabel = [[UILabel alloc] init];
         self.dateTimeLabel.backgroundColor = [UIColor clearColor];
-        self.dateTimeLabel.font = [UIFont systemFontOfSize:14.f];
+        self.dateTimeLabel.font = [UIFont systemFontOfSize:12.f];
         self.dateTimeLabel.textColor = [UIColor colorWithRed:0xb4 / 255.f green:0xb4 / 255.f blue:0xb4 / 255.f alpha:1.f];
         self.dateTimeLabel.numberOfLines = 1;
         self.dateTimeLabel.opaque = YES;
@@ -58,13 +58,13 @@
     [super layoutSubviews];
     
     CGSize dateTimeSize = [self.dateTimeLabel.text sizeWithFont:self.dateTimeLabel.font constrainedToSize:CGSizeMake(80.0f, self.dateTimeLabel.font.pointSize)];
-    self.dateTimeLabel.frame = CGRectMake(self.contentView.frame.size.width - 7.f - dateTimeSize.width, (self.contentView.frame.size.height - dateTimeSize.height) / 2.f, dateTimeSize.width, dateTimeSize.height);
+    self.dateTimeLabel.frame = CGRectMake(self.contentView.frame.size.width - dateTimeSize.width, (self.contentView.frame.size.height - dateTimeSize.height) / 2.f, dateTimeSize.width, dateTimeSize.height);
 
     CGSize nameSize = [self.nameLabel.text sizeWithFont:self.nameLabel.font constrainedToSize:CGSizeMake(self.dateTimeLabel.frame.origin.x - 16.f, self.nameLabel.font.pointSize)];
-    self.nameLabel.frame = CGRectMake(16.f, 4.f, nameSize.width, nameSize.height);
+    self.nameLabel.frame = CGRectMake(29.f, 6.f, nameSize.width, nameSize.height);
     
     CGSize phoneTypeSize = [self.descriptionLabel.text sizeWithFont:self.descriptionLabel.font constrainedToSize:CGSizeMake(self.dateTimeLabel.frame.origin.x, self.descriptionLabel.font.pointSize)];
-    self.descriptionLabel.frame = CGRectMake(self.nameLabel.frame.origin.x, self.contentView.frame.size.height - phoneTypeSize.height - 4.f, phoneTypeSize.width, phoneTypeSize.height);
+    self.descriptionLabel.frame = CGRectMake(self.nameLabel.frame.origin.x, self.contentView.frame.size.height - phoneTypeSize.height - 8.f, phoneTypeSize.width, phoneTypeSize.height);
     
     if (self.iconImageView.image) {
         CGSize imageSize = self.iconImageView.image.size;
