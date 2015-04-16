df5660e5b27fe8a2fe329775443a2c9d7aba3098
Reinier Wieringa <reinier@astralvision.nl>
2015-04-16 21:57:47 +0200

    VIALI-26 Gradient background implemented

---
+++ Vialer/GradientView.h
@@ -0,0 +1,13 @@
+//
+//  GradientView.h
+//  Vialer
+//
+//  Created by Reinier Wieringa on 15/04/15.
+//  Copyright (c) 2015 VoIPGRID. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GradientView : UIView
+
+@end
