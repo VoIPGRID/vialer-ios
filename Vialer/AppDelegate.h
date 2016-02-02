d9d436f8eaeba98bc3016e043968c987c1df0a98
Harold <harold.dijkstra@wearespindle.com>
2016-02-02 13:31:07 +0100

    VIALI-3105 Created an APNS Handler class responsible for APNS registration and implementation. - Created tests for the APNS Handler.

--- Vialer/AppDelegate.h
+++ Vialer/AppDelegate.h
@@ -4,7 +4,6 @@
 //
 
 #import <UIKit/UIKit.h>
-#import <PushKit/PushKit.h>
 
 @class GSCall;
 
--- Vialer/AppDelegate.m
+++ Vialer/AppDelegate.m
@@ -11,15 +11,10 @@
 #endif
 #import "SSKeychain.h"
 
+#import "APNSHandler.h"
 #import "GAITracker.h"
 #import "PZPushMiddleware.h"
 #import "SystemUser.h"
-#import "VoIPGRIDRequestOperationManager.h"
-
-#define VOIP_TOKEN_STORAGE_KEY @"VOIP-TOKEN"
-
-@interface AppDelegate() <PKPushRegistryDelegate>
-@end
 
 @implementation AppDelegate
 
@@ -28,6 +23,8 @@
 - (BOOL)application:(UIApplication *)application willFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     [SSKeychain setAccessibilityType:kSecAttrAccessibleAfterFirstUnlock];
     [application setMinimumBackgroundFetchInterval:UIApplicationBackgroundFetchIntervalMinimum];
+
+    [[APNSHandler sharedHandler] registerForVoIPNotifications];
 
     //Only when the app is run for screenshot purposes do the following:
     if ([[self class] isSnapshotScreenshotRun]) {
@@ -71,39 +68,16 @@
 
 #pragma mark - UIApplication notification delegate
 
-- (void)application:(UIApplication *)application handleActionWithIdentifier:(NSString *)identifier forLocalNotification:(UILocalNotification *)notification completionHandler:(void (^)()) completionHandler {
-    NSLog(@"Received push notification: %@, identifier: %@", notification, identifier); // iOS 8
-    if (application.applicationState != UIApplicationStateActive) {
-    }
-}
-
-- (void)application:(UIApplication *)application didReceiveLocalNotification:(UILocalNotification *)notification {
-    if (application.applicationState != UIApplicationStateActive) { //
-    }
-}
-
-#pragma mark - PKPushRegistray management
-- (void)pushRegistry:(PKPushRegistry *)registry didReceiveIncomingPushWithPayload:(PKPushPayload *)payload forType:(NSString *)type {
-    NSLog(@"%s Incoming push notification of type: %@", __PRETTY_FUNCTION__, type);
-    UIApplicationState state = [[UIApplication sharedApplication] applicationState];
-    NSDictionary *payloadDict = [payload dictionaryPayload];
-    [[PZPushMiddleware sharedInstance] handleReceivedNotificationForApplicationState:state payload:payloadDict];
-}
-
-- (void)pushRegistry:(PKPushRegistry *)registry didUpdatePushCredentials:(PKPushCredentials *)credentials forType:(NSString *)type {
-    NSLog(@"Registration successful, bundle identifier: %@, device token: %@", [NSBundle.mainBundle bundleIdentifier], credentials.token);
-    if (credentials.token) {
-        [[PZPushMiddleware sharedInstance] updateDeviceRecordForToken:credentials.token];
-    }
-}
-
-- (void)pushRegistry:(PKPushRegistry *)registry didInvalidatePushTokenForType:(NSString *)type {
-    NSData *token = [registry pushTokenForType:type];
-    if (token) {
-        [[PZPushMiddleware sharedInstance] unregisterToken:[PZPushMiddleware deviceTokenStringFromData:token]
-                                             andSipAccount:[SystemUser currentUser].sipAccount];
-    }
-}
+//- (void)application:(UIApplication *)application handleActionWithIdentifier:(NSString *)identifier forLocalNotification:(UILocalNotification *)notification completionHandler:(void (^)()) completionHandler {
+//    NSLog(@"Received push notification: %@, identifier: %@", notification, identifier); // iOS 8
+//    if (application.applicationState != UIApplicationStateActive) {
+//    }
+//}
+//
+//- (void)application:(UIApplication *)application didReceiveLocalNotification:(UILocalNotification *)notification {
+//    if (application.applicationState != UIApplicationStateActive) { //
+//    }
+//}
 
 #pragma mark - Handle person(s) & calls
 - (void)handleSipCall:(GSCall *)sipCall {
