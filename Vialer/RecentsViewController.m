3242f185f7bcb3155986cc4429d56a5e79fb16c9
Bob Voorneveld <bob.voorneveld@wearespindle.com>
2016-06-07 15:10:18 +0200

    VIALI-3201: Make sure 2 step is possible on highspeed internet and calling recent

--- Vialer/RecentsViewController.m
+++ Vialer/RecentsViewController.m
@@ -367,14 +367,14 @@ static NSTimeInterval const RecentsViewControllerReachabilityBarAnimationDuratio
     if (self.reachabilityStatus == ReachabilityManagerStatusHighSpeed && [SystemUser currentUser].sipEnabled) {
         [GAITracker setupOutgoingSIPCallEvent];
         [self performSegueWithIdentifier:RecentViewControllerSIPCallingSegue sender:self];
-    } else if (self.reachabilityStatus == ReachabilityManagerStatusLowSpeed) {
-        [GAITracker setupOutgoingConnectABCallEvent];
-        [self performSegueWithIdentifier:RecentViewControllerTwoStepCallingSegue sender:self];
-    } else {
+    } else if (self.reachabilityStatus == ReachabilityManagerStatusOffline) {
         UIAlertController *alert = [UIAlertController alertControllerWithTitle:NSLocalizedString(@"No internet connection", nil)
                                                                        message:NSLocalizedString(@"It's not possible to setup a call. Make sure you have an internet connection.", nil)
                                                           andDefaultButtonText:NSLocalizedString(@"Ok", nil)];
         [self presentViewController:alert animated:YES completion:nil];
+    } else {
+        [GAITracker setupOutgoingConnectABCallEvent];
+        [self performSegueWithIdentifier:RecentViewControllerTwoStepCallingSegue sender:self];
     }
 }
 
