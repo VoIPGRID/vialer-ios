ca0070aaa8befdc1fa28957bc579f8ddff3bb1f5
Chris Kontos <chris.kontos@wearespindle.com>
2018-11-13 18:17:32 +0100

    VIALI-3529: Fixed and updated Middleware unit tests with the least necessary changes on the project

--- VialerTests/MiddlewareRequestOperationManagerTests.m
+++ VialerTests/MiddlewareRequestOperationManagerTests.m
@@ -3,7 +3,7 @@
 //  Copyright © 2016 VoIPGRID. All rights reserved.
 //
 
-#import "Configuration.h"
+#import "Vialer-Swift.h"
 #import "Middleware.h"
 #import "MiddlewareRequestOperationManager.h"
 #import <OCMock/OCMock.h>
@@ -23,11 +23,11 @@
 
 - (void)setUp {
     [super setUp];
-
+    
     //To ensure an equal middleware API setup as actually used by the middleware class.
     Middleware *aMiddlewareInstance = [[Middleware alloc] init];
     self.middlewareRequestOperationManager = [aMiddlewareInstance commonMiddlewareRequestOperationManager];
-    self.middlewareBaseURLAsString = [[Configuration defaultConfiguration] UrlForKey:ConfigurationMiddleWareBaseURLString];
+    self.middlewareBaseURLAsString = [[UrlsConfiguration shared] middlewareBaseUrl];
 }
 
 - (void)tearDown {
@@ -42,7 +42,7 @@
     NSString *mockAPNSToken = @"0000000011111111222222223333333344444444555555556666666677777777";
     NSString *mockSIPAccount = @"42";
     int statusCodeToReturn = 502;
-
+    
     [OHHTTPStubs stubRequestsPassingTest:^BOOL(NSURLRequest *request) {
         // Match the requested url with the url from the middleware as per configuration class/plist
         NSString *requestSchemeAndHost = [NSString stringWithFormat:@"%@://%@", request.URL.scheme, request.URL.host];
@@ -50,9 +50,9 @@
     } withStubResponse:^OHHTTPStubsResponse *(NSURLRequest *request) {
         return [OHHTTPStubsResponse responseWithData:[NSData data] statusCode:statusCodeToReturn headers:nil];
     }];
-
+    
     XCTestExpectation *expectation = [self expectationWithDescription:@"Creating device record"];
-
+    
     //When
     [self.middlewareRequestOperationManager updateDeviceRecordWithAPNSToken:mockAPNSToken sipAccount:mockSIPAccount withCompletion:^(NSError *error) {
         if (!error) {
@@ -65,7 +65,7 @@
         }
         [expectation fulfill];
     }];
-
+    
     [self waitForExpectationsWithTimeout:5.0 handler:^(NSError *error) {
         if (error) {
             XCTFail(@"Expectation error");
@@ -79,7 +79,7 @@
                               @"message_start_time" : @1234,
                               };
     int statusCodeToReturn = VoIPGRIDHttpErrorRequestTimeout;
-
+    
     [OHHTTPStubs stubRequestsPassingTest:^BOOL(NSURLRequest *request) {
         // Match the requested url with the url from the middleware as per configuration class/plist
         NSString *requestSchemeAndHost = [NSString stringWithFormat:@"%@://%@", request.URL.scheme, request.URL.host];
@@ -87,9 +87,9 @@
     } withStubResponse:^OHHTTPStubsResponse *(NSURLRequest *request) {
         return [OHHTTPStubsResponse responseWithData:[NSData data] statusCode:statusCodeToReturn headers:nil];
     }];
-
+    
     XCTestExpectation *expectation = [self expectationWithDescription:@"Call response to middleware"];
-
+    
     //When
     [self.middlewareRequestOperationManager sentCallResponseToMiddleware:payload isAvailable:NO withCompletion:^(NSError *error) {
         if (!error) {
@@ -99,11 +99,11 @@
             NSHTTPURLResponse *response = [[error userInfo] objectForKey:AFNetworkingOperationFailingURLResponseErrorKey];
             XCTAssert(response.statusCode == statusCodeToReturn,
                       @"Unexpected HTTP status code returned, expected: %d, received: %ld", statusCodeToReturn, (long)response.statusCode);
-
+            
         }
         [expectation fulfill];
     }];
-
+    
     [self waitForExpectationsWithTimeout:5.0 handler:^(NSError *error) {
         if (error) {
             XCTFail(@"Expectation error");
