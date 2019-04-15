7266aa384fa461850d8d6672fc5cda7d52512a54
Arjen Vellinga <arjen.vellinga@wearespindle.com>
2019-04-15 16:27:51 +0200

    Fallback to default sample rate

--- Vialer/Calling/SIP/Utils/SIPUtils.m
+++ Vialer/Calling/SIP/Utils/SIPUtils.m
@@ -162,8 +162,7 @@
                                            [[VSLAudioCodecs alloc] initWithAudioCodec:VSLAudioCodecOpus andPriority:210]
                                            ];
         VSLOpusConfiguration *opusConfiguration = [[VSLOpusConfiguration alloc] init];
-        opusConfiguration.sampleRate = VSLOpusConfigurationSampleRateWideBand;
-        opusConfiguration.frameDuration = VSLOpusConfigurationFrameDurationTwenty;
+        opusConfiguration.frameDuration = VSLOpusConfigurationFrameDurationTwenty;        
 
         codecConfiguration.opusConfiguration = opusConfiguration;
     } else {
