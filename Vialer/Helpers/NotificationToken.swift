677e7004241b1803a4e3b9c158f1fae60106c6e0
Chris Kontos <chris.kontos@wearespindle.com>
2019-12-09 00:16:35 +0100

    VIALI-3812: Improve reachability bar

--- Vialer/Helpers/NotificationToken.swift
+++ Vialer/Helpers/NotificationToken.swift
@@ -52,9 +52,10 @@ extension NotificationCenter {
 
 extension SystemUser {
     static var logoutNotification = NotificationDescriptor<Any>(name: Notification.Name.SystemUserLogout)
-    static var sipChangedNotification = NotificationDescriptor<Any>(name: Notification.Name.SystemUserSIPDisabled)
-    static var sipDisabledNotification = NotificationDescriptor<Any>(name: Notification.Name.SystemUserSIPCredentialsChanged)
+    static var sipDisabledNotification = NotificationDescriptor<Any>(name: Notification.Name.SystemUserSIPDisabled)
+    static var sipChangedNotification = NotificationDescriptor<Any>(name: Notification.Name.SystemUserSIPCredentialsChanged)
     static var use3GPlusNotification = NotificationDescriptor<Any>(name: Notification.Name.SystemUserUse3GPlus)
+    static var encryptionUsageNotification = NotificationDescriptor<Any>(name: Notification.Name.SystemUserEncryptionUsageChanged)
 }
 
 extension Middleware {
