96c6fd1387bedeee91a0df36469b005e13d85b8a
Arjen Vellinga <arjen.vellinga@wearespindle.com>
2019-12-19 10:48:46 +0100

    Handle VG api date with or without fractional seconds, removed unused code, improved naming

--- VialerTests/RecentsTimeConverterTests.swift
+++ VialerTests/RecentsTimeConverterTests.swift
@@ -24,56 +24,6 @@ class RecentsTimeConverterTests: XCTestCase {
         return formatter
     }
 
-
-    /**
-     Test creates an "API expected time string" from the given components and
-     verifies that a correct NSDate object representing this time string is created.
-     */
-    func test24hCETstringGivesCorrectDate() {
-        // Given
-        var components = DateComponents()
-        components.year = 2009
-        components.month = 10
-        components.day = 11
-        components.hour = 12
-        components.minute = 13
-        components.second = 14
-
-        // Constructs a 24h CET time string as expected by the API in the format
-        // yyyy-MM-dd'T'HH:mm:ss (this seems to be ISO-8601)
-        let string24hCET = "\(components.year!)-\(components.month!)-\(components.day!)T\(components.hour!):\(components.minute!):\(components.second!)"
-
-        // When
-        let receivedDate = recentsTimeConverter.dateFrom24hCET(timeString: string24hCET)
-
-        // Then
-        let expectedDate = calendar.date(from: components)!
-        XCTAssert(receivedDate == expectedDate)
-    }
-
-    /**
-     Test creates an NSDate object from the given components and verifies that a
-     correct "API expected time string" is returned.
-     */
-    func testClassGivesAPI24hCETStringFromGivenDate() {
-        // Given
-        var components = DateComponents()
-        components.year = 2009
-        components.month = 10
-        components.day = 11
-        components.hour = 12
-        components.minute = 13
-        components.second = 14
-        let dateToConvert = calendar.date(from: components)
-
-        // When
-        let receivedAPIformatted24hCETstring = recentsTimeConverter.apiFormatted24hCETstringFrom(date: dateToConvert!)
-
-        // Then
-        let expected24hAPIstring = "\(components.year!)-\(components.month!)-\(components.day!)T\(components.hour!):\(components.minute!):\(components.second!)"
-        XCTAssert(receivedAPIformatted24hCETstring == expected24hAPIstring)
-    }
-
     /**
      Passing a time one second AFTER midnight e.g. this day, the test verifies
      that the relative Day/Time string returned is a time (hh:mm)
